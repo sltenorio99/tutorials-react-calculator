74c6a354870c866412b6888eef0168cd8082677e
Shane-Lewes Tenorio <sltenorio99@gmail.com>
2019-05-22 21:39:48 -0500

    🎉📝 Create Keypad component and add/configure tests

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,42 +1,51 @@
-import React, { Component } from 'react';
-import Display from '../Display/Display';
-import './Calculator.css';
+import React, { Component } from "react";
+import Display from "../Display/Display";
+import Keypad from "../Keypad/Keypad";
+import "./Calculator.css";
 
 class Calculator extends Component {
   state = {
     // value to be displayed in <Display />
-    displayValue: '0',
+    displayValue: "0",
     // values to be displayed in number <Keys />
     numbers: [],
     // values to be displayed in operator <Keys />
     operators: [],
     // operator selected for math operation
-    selectedOperator: '',
+    selectedOperator: "",
     // stored value to use for math operation
-    storedValue: ''
-  }
+    storedValue: ""
+  };
 
   callOperator = () => {
-    console.log('call operation');
-  }
+    console.log("call operation");
+  };
 
   setOperator = () => {
-    console.log('set operation');
-  }
+    console.log("set operation");
+  };
 
   updateDisplay = () => {
-    console.log('update display');
-  }
+    console.log("update display");
+  };
 
   render = () => {
-    const { displayValue } = this.state;
+    // destructure Calculator state
+    const { displayValue, numbers, operators } = this.state;
 
     return (
       <div className="calculator-container">
         <Display displayValue={displayValue} />
+        <Keypad
+          callOperator={this.callOperator}
+          numbers={numbers}
+          operators={operators}
+          setOperator={this.setOperator}
+          updateDisplay={this.updateDisplay}
+        />
       </div>
-    )
-  }
+    );
+  };
 }
 
 export default Calculator;
--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Calculator from './Calculator';
-import Display from '../Display/Display';
+import React from "react";
+import { shallow } from "enzyme";
+import Calculator from "./Calculator";
+import Display from "../Display/Display";
+import Keypad from "../Keypad/Keypad";
 
-describe('Calculator', () => {
+describe("Calculator", () => {
   let wrapper;
-  beforeEach(() => wrapper = shallow(<Calculator/>));
+  beforeEach(() => (wrapper = shallow(<Calculator />)));
 
-  it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+  it("should render a <div />", () => {
+    expect(wrapper.find("div").length).toEqual(1);
   });
 
-  it('should render the Display component', () => {
-    expect(wrapper.containsMatchingElement(
-      <Display displayValue={wrapper.instance().state.displayValue}/>)
+  it("should render the Display and Keypad components", () => {
+    expect(
+      wrapper.containsAllMatchingElements([
+        <Display displayValue={wrapper.instance().state.displayValue} />,
+        <Keypad
+          callOperator={wrapper.instance().callOperator}
+          numbers={wrapper.instance().state.numbers}
+          operators={wrapper.instance().state.operators}
+          setOperator={wrapper.instance().setOperator}
+          updateDisplay={wrapper.instance().updateDisplay}
+        />
+      ])
     ).toEqual(true);
-  })
-
+  });
 });
